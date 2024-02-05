0be474fcdfc2cd42ffd2a8a956219eae4392a663
Roberto Jacobo <robjacobox@gmail.com>
2024-02-05 01:57:31 -0600

    sidebar behavior - click improved (#16)
    Co-authored-by: ¨roberjacobo¨ <¨robjacobox@gmail.com¨>

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,45 +1,76 @@
-// sidebar.component.ts
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-    selector: 'shared-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.css'],
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-    items: MenuItem[] = [];
-    sidebarVisible: boolean = false;
+  items: MenuItem[] = [];
+  sidebarVisible: boolean = false;
 
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'Home',
-                icon: 'pi pi-fw pi-home',
-                routerLink: '/'
-            },
-            {
-                label: 'Experience',
-                icon: 'pi pi-fw pi-briefcase',
-                items: [
-                    {
-                        label: 'Technologies',
-                        icon: 'pi pi-fw pi-code',
-                        routerLink: '/experience',
-                        fragment: 'technologies'
-                    },
-                    {
-                        label: 'Companies',
-                        icon: 'pi pi-fw pi-building',
-                        routerLink: '/experience',
-                        fragment: 'companies'
-                    },
-                ],
-            },
-            {
-                label: 'Work Networks',
-                icon: 'pi pi-fw pi-github',
-            },
-        ];
+  constructor() {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/'
+      },
+      {
+        label: 'Experience',
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+          {
+            label: 'Technologies',
+            icon: 'pi pi-fw pi-code',
+            routerLink: '/experience',
+            fragment: 'technologies'
+          },
+          {
+            label: 'Companies',
+            icon: 'pi pi-fw pi-building',
+            routerLink: '/experience',
+            fragment: 'companies'
+          },
+        ],
+      },
+      {
+        label: 'Work Networks',
+        icon: 'pi pi-fw pi-github',
+      },
+    ];
+  }
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+    if (this.sidebarVisible) {
+      // Añade el manejador de eventos para deshabilitar el scroll fuera del sidebar
+      document.body.style.overflow = 'hidden';
+      window.addEventListener('wheel', this.disableScroll, { passive: false });
+    } else {
+      // Remueve el manejador de eventos cuando el sidebar se oculta
+      // y asegura que el desplazamiento se habilita nuevamente
+      document.body.style.overflow = '';
+      window.removeEventListener('wheel', this.disableScroll);
     }
+  }
+  
+
+  disableScroll = (event: WheelEvent) => {
+    if (this.sidebarVisible) {
+      event.preventDefault();
+    }
+  };
+
+  onSidebarHide() {
+    // Asegura que el desplazamiento se habilita nuevamente al cerrar el sidebar
+    document.body.style.overflow = '';
+    window.removeEventListener('wheel', this.disableScroll);
+    // Opcionalmente, restablece sidebarVisible a false si es necesario
+    this.sidebarVisible = false;
+  }
+  
 }
