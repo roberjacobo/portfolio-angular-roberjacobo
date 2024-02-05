cba59da79a0ce26a5043069532ea0f2b09b806fa
¨roberjacobo¨ <robjacobox@gmail.com>
2024-02-05 15:04:38 -0600

    sidebar updated

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -2,75 +2,95 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'shared-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css'],
+	selector: 'shared-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-  items: MenuItem[] = [];
-  sidebarVisible: boolean = false;
+	items: MenuItem[] = [];
+	sidebarVisible: boolean = false;
 
-  constructor() {}
+	constructor() {}
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Home',
-        icon: 'pi pi-fw pi-home',
-        routerLink: '/'
-      },
-      {
-        label: 'Experience',
-        icon: 'pi pi-fw pi-briefcase',
-        items: [
-          {
-            label: 'Technologies',
-            icon: 'pi pi-fw pi-code',
-            routerLink: '/experience',
-            fragment: 'technologies'
-          },
-          {
-            label: 'Companies',
-            icon: 'pi pi-fw pi-building',
-            routerLink: '/experience',
-            fragment: 'companies'
-          },
-        ],
-      },
-      {
-        label: 'Work Networks',
-        icon: 'pi pi-fw pi-github',
-      },
-    ];
-  }
-
-  toggleSidebar() {
-    this.sidebarVisible = !this.sidebarVisible;
-    if (this.sidebarVisible) {
-      // Añade el manejador de eventos para deshabilitar el scroll fuera del sidebar
-      document.body.style.overflow = 'hidden';
-      window.addEventListener('wheel', this.disableScroll, { passive: false });
-    } else {
-      // Remueve el manejador de eventos cuando el sidebar se oculta
-      // y asegura que el desplazamiento se habilita nuevamente
-      document.body.style.overflow = '';
-      window.removeEventListener('wheel', this.disableScroll);
-    }
-  }
+	ngOnInit() {
+		this.items = [
+			{
+				label: 'Home',
+				icon: 'pi pi-fw pi-home',
+				routerLink: '/',
+				expanded: false,
+				items: [
+					{
+						label: 'about',
+						icon: 'pi pi-user',
+						routerLink: '/',
+						fragment: 'about'
+					}
+				],
+			},
+			{
+				label: 'Experience',
+				icon: 'pi pi-fw pi-briefcase',
+				expanded: false,
+				items: [
+					{
+						label: 'Technologies',
+						icon: 'pi pi-fw pi-code',
+						routerLink: '/experience',
+						fragment: 'technologies'
+					},
+					{
+						label: 'Companies',
+						icon: 'pi pi-fw pi-building',
+						routerLink: '/experience',
+						fragment: 'companies'
+					},
+				],
+			},
+			{
+				label: 'Work Networks',
+				icon: 'pi pi-external-link',
+				routerLink: '/networks',
+				expanded: false,
+				items: [
+					{
+						label: 'Github',
+						icon: 'pi pi-fw pi-github',
+						externalLink: 'https://github.com/roberjacobo',
+					}
+				],
+			},
+		];
+	}
   
 
-  disableScroll = (event: WheelEvent) => {
-    if (this.sidebarVisible) {
-      event.preventDefault();
-    }
-  };
+	toggleSidebar() {
+		this.sidebarVisible = !this.sidebarVisible;
+		if (this.sidebarVisible) {
+			// Añade el manejador de eventos para deshabilitar el scroll fuera del sidebar
+			document.body.style.overflow = 'hidden';
+			window.addEventListener('wheel', this.disableScroll, { passive: false });
+		} else {
+			// Remueve el manejador de eventos cuando el sidebar se oculta
+			// y asegura que el desplazamiento se habilita nuevamente
+			document.body.style.overflow = '';
+			window.removeEventListener('wheel', this.disableScroll);
+		}
+	}
+  
 
-  onSidebarHide() {
-    // Asegura que el desplazamiento se habilita nuevamente al cerrar el sidebar
-    document.body.style.overflow = '';
-    window.removeEventListener('wheel', this.disableScroll);
-    // Opcionalmente, restablece sidebarVisible a false si es necesario
-    this.sidebarVisible = false;
-  }
+	disableScroll = (event: WheelEvent) => {
+		if (this.sidebarVisible) {
+			event.preventDefault();
+		}
+	};
+
+	onSidebarHide() {
+		// Asegura que el desplazamiento se habilita nuevamente al cerrar el sidebar
+		document.body.style.overflow = '';
+		window.removeEventListener('wheel', this.disableScroll);
+		// Opcionalmente, restablece sidebarVisible a false si es necesario
+		this.sidebarVisible = false;
+	}
   
 }
