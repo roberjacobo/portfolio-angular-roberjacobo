e9ceff3a40d8e059648867f5eb014a092ce45512
¨roberjacobo¨ <robjacobox@gmail.com>
2024-02-08 17:53:06 -0600

    sidebar updated

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,96 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { Renderer2 } from '@angular/core';
 
 @Component({
-	selector: 'shared-sidebar',
-	templateUrl: './sidebar.component.html',
-	styleUrls: ['./sidebar.component.css'],
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarComponent implements OnInit {
-	items: MenuItem[] = [];
-	sidebarVisible: boolean = false;
+export class SidebarComponent implements OnInit, OnDestroy {
+  items: MenuItem[] = [];
+  sidebarVisible: boolean = false;
 
-	constructor() {}
+  constructor(private renderer: Renderer2) {}
 
-	ngOnInit() {
-		this.items = [
-			{
-				label: 'Home',
-				icon: 'pi pi-fw pi-home',
-				routerLink: '/',
-				expanded: false,
-				items: [
-					{
-						label: 'about',
-						icon: 'pi pi-user',
-						routerLink: '/',
-						fragment: 'about'
-					}
-				],
-			},
-			{
-				label: 'Experience',
-				icon: 'pi pi-fw pi-briefcase',
-				expanded: false,
-				items: [
-					{
-						label: 'Technologies',
-						icon: 'pi pi-fw pi-code',
-						routerLink: '/experience',
-						fragment: 'technologies'
-					},
-					{
-						label: 'Companies',
-						icon: 'pi pi-fw pi-building',
-						routerLink: '/experience',
-						fragment: 'companies'
-					},
-				],
-			},
-			{
-				label: 'Work Networks',
-				icon: 'pi pi-external-link',
-				routerLink: '/networks',
-				expanded: false,
-				items: [
-					{
-						label: 'Github',
-						icon: 'pi pi-fw pi-github',
-						externalLink: 'https://github.com/roberjacobo',
-					}
-				],
-			},
-		];
-	}
-  
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/',
+        expanded: false,
+        items: [
+          {
+            label: 'about',
+            icon: 'pi pi-user',
+            routerLink: '/',
+            fragment: 'about'
+          }
+        ],
+      },
+      {
+        label: 'Experience',
+        icon: 'pi pi-fw pi-briefcase',
+        expanded: false,
+        items: [
+          {
+            label: 'Technologies',
+            icon: 'pi pi-fw pi-code',
+            routerLink: '/experience',
+            fragment: 'technologies'
+          },
+          {
+            label: 'Companies',
+            icon: 'pi pi-fw pi-building',
+            routerLink: '/experience',
+            fragment: 'companies'
+          },
+        ],
+      },
+      {
+        label: 'Work Networks',
+        icon: 'pi pi-external-link',
+        routerLink: '/networks',
+        expanded: false,
+        items: [
+          {
+            label: 'Github',
+            icon: 'pi pi-fw pi-github',
+            externalLink: 'https://github.com/roberjacobo',
+          },
+		  {
+			label: 'LinkedIn',
+			icon: 'pi pi-linkedin',
+			externalLink: 'https://www.linkedin.com/in/roberto-jacobo/'
+		  }
+        ],
+      },
+    ];
+  }
 
-	toggleSidebar() {
-		this.sidebarVisible = !this.sidebarVisible;
-		if (this.sidebarVisible) {
-			// Añade el manejador de eventos para deshabilitar el scroll fuera del sidebar
-			document.body.style.overflow = 'hidden';
-			window.addEventListener('wheel', this.disableScroll, { passive: false });
-		} else {
-			// Remueve el manejador de eventos cuando el sidebar se oculta
-			// y asegura que el desplazamiento se habilita nuevamente
-			document.body.style.overflow = '';
-			window.removeEventListener('wheel', this.disableScroll);
-		}
-	}
-  
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+    if (this.sidebarVisible) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+      window.addEventListener('wheel', this.disableScroll, { passive: false });
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+      window.removeEventListener('wheel', this.disableScroll);
+    }
+  }
 
-	disableScroll = (event: WheelEvent) => {
-		if (this.sidebarVisible) {
-			event.preventDefault();
-		}
-	};
+  disableScroll = (event: WheelEvent) => {
+    if (this.sidebarVisible) {
+      event.preventDefault();
+    }
+  };
 
-	onSidebarHide() {
-		// Asegura que el desplazamiento se habilita nuevamente al cerrar el sidebar
-		document.body.style.overflow = '';
-		window.removeEventListener('wheel', this.disableScroll);
-		// Opcionalmente, restablece sidebarVisible a false si es necesario
-		this.sidebarVisible = false;
-	}
-  
+  onSidebarHide() {
+    this.renderer.removeStyle(document.body, 'overflow');
+    window.removeEventListener('wheel', this.disableScroll);
+    this.sidebarVisible = false;
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('wheel', this.disableScroll);
+  }
+
+  // Ajuste de trackByFn para tipos explícitos
+  trackByFn(index: number, item: MenuItem): string {
+    // Asume que cada item tiene una propiedad 'label' que es única
+    // Si tienes un identificador único, úsalo aquí en su lugar
+    return item.label ?? 'defaultLabel'; // Asegúrate de que 'label' es único entre todos tus items
+  }
 }
